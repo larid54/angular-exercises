30fd2c30bbeca8c98b65e1a484426ce61745ca02
larid54 <gurgulpatryk7@gmail.com>
2015-08-14 09:42:47 +0200

    exrcise2 angular

--- app/BlogPostCtrl.js
+++ app/BlogPostCtrl.js
@@ -1,11 +1,18 @@
 (function ()
 {
     'use strict';
-    function BlogPostCtrl(PostDAO)
-    {
+    function BlogPostCtrl(PostDAO) {
+        var ctrl = this;
+        ctrl.tab1=[];
+        ctrl.tab2=[];
+        PostDAO.query().then(function (data) {
+            ctrl.posts=data;
 
+
+
+
+        })
     }
-
     var module = angular.module('exerciseApp');
     module.controller('BlogPostCtrl', ['PostDAO', BlogPostCtrl]);
 })();
